f4d19ac3e85e7aa3d1f8a95b6f225811e4045412
Padjar Ekayuna <ganzcrn@gmail.com>
2024-07-09 15:09:12 +0700

    first commit

---
+++ Main.py
@@ -0,0 +1,49 @@
+# Variabel adalah tempat menyimpan data
+
+# menaru / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+# di atas adalah contoh penulisan variabel
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+#diatas adalah cara pemanggilan atau untuk
+#menampilkan variabel
+
+# penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+# Walaupun sudah diisi variabel a dengan
+# angka 10 tapi kita bisa mengubahnya lagi di pemanggilan kedua
+# kita langsung beri saja nilai nya seperti angka 7 di atas
+# jadi var a yang tadinya 10 menjadi 7
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
+# Nah kalo ini si var a yang nilai nya 7 tadi kita masukan lagi
+# kedalam variabel yaitu b, yg mana ketika kita panggil si var b
+# maka nilai nya akan sama dengan var a yaitu 7.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
